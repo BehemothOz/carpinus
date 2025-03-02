d21e90022341374e514c4c9d6b6e55155292e892
behemothoz <behemothoz@yandex.ru>
2025-03-02 03:48:19 +0300

    emitter types

--- src/core/EventEmitter.ts
+++ src/core/EventEmitter.ts
@@ -1,19 +1,19 @@
-export class EventEmitter {
+export class EventEmitter<T extends object> {
     private target: EventTarget;
 
     constructor() {
         this.target = new EventTarget();
     }
 
-    public on<T>(event: string, listener: (event: CustomEvent<T>) => void): void {
-        this.target.addEventListener(event, listener as EventListener);
+    public on<K extends keyof T>(event: K, listener: (event: CustomEvent<T[K]>) => void): void {
+        this.target.addEventListener(event.toString(), listener as EventListener);
     }
 
-    public off<T>(event: string, listener: (event: CustomEvent<T>) => void): void {
-        this.target.removeEventListener(event, listener as EventListener);
+    public off<K extends keyof T>(event: string, listener: (event: CustomEvent<T[K]>) => void): void {
+        this.target.removeEventListener(event.toString(), listener as EventListener);
     }
 
-    public emit<T>(event: string, detail?: T): void {
+    public emit<K extends keyof T>(event: string, detail?: T[K]): void {
         const customEvent = new CustomEvent(event, { detail });
         this.target.dispatchEvent(customEvent);
     }
