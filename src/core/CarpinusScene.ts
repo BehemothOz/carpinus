cb798748d42ade8d5e20ad1a8c831a6d0efa4313
behemothoz <behemothoz@yandex.ru>
2025-03-03 17:46:40 +0300

    draw scene

--- src/core/CarpinusScene.ts
+++ src/core/CarpinusScene.ts
@@ -1,13 +1,14 @@
 import { Scene, type SceneOptions } from './scene/Scene';
 import { Rectangle } from './scheme/shapes/Rectangle';
+import { Line } from './scheme/shapes/Line';
 import { SourceTreeNode } from './scheme/SourceTreeNode';
 
 interface CarpinusSceneOptions extends SceneOptions {
-    dataScheme: SourceTreeNode[];
+    dataScheme: SourceTreeNode;
 }
 
 export class CarpinusScene extends Scene {
-    dataScheme: any;
+    dataScheme: SourceTreeNode;
 
     constructor(options: CarpinusSceneOptions) {
         super(options);
@@ -16,16 +17,40 @@ export class CarpinusScene extends Scene {
     }
 
     protected drawScene(): void {
-        for (const node of this.dataScheme) {
-            const rectangle = new Rectangle({
+        this.drawScheme(this.dataScheme);
+    }
+
+    private drawScheme(node: SourceTreeNode) {
+        const { position, size, children } = node;
+
+        const rectangle = new Rectangle({
+            ctx: this.ctx,
+            x: position.x,
+            y: position.y,
+            width: size.width,
+            height: size.height,
+        });
+
+        if (children.length > 0) {
+            const lastChildNode = children.at(-1) as SourceTreeNode;
+
+            const startX = position.x + size.width / 2;
+            const startY = position.y + size.height;
+
+            const line = new Line({
                 ctx: this.ctx,
-                x: node.position.x,
-                y: node.position.y,
-                width: node.size.width,
-                height: node.size.height,
+                x: startX,
+                y: startY,
             });
 
-            rectangle.draw();
+            children.forEach((child) => {
+                this.drawScheme(child);
+            });
+
+            line.lineTo(lastChildNode.position.x, lastChildNode.position.y);
+            line.draw();
         }
+
+        rectangle.draw();
     }
 }
