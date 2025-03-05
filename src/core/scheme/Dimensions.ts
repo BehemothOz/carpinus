e7bb7bb9c23631bb59d28a8877ea5dc6b18276a7
behemothoz <behemothoz@yandex.ru>
2025-03-05 21:35:58 +0300

    new dimension

--- src/core/scheme/Dimensions.ts
+++ src/core/scheme/Dimensions.ts
@@ -8,6 +8,7 @@ abstract class Dimensions {
 export class Position extends Dimensions {}
 export class Offset extends Dimensions {}
 export class Gap extends Dimensions {}
+export class Point extends Dimensions {}
 
 export class Size {
     constructor(public width: number, public height: number) {
@@ -15,3 +16,13 @@ export class Size {
         this.height = height;
     }
 }
+
+export type Level = number;
+
+export interface LevelSize extends Size {}
+
+export interface NodeLevelMeasure extends Size {
+    level: Level;
+}
+
+export interface SchemeMeasure extends Size {}
