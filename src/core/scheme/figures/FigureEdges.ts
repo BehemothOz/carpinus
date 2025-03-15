a848bee280272c3bb8e05d84e43ce399e7e9ffce
behemothoz <behemothoz@yandex.ru>
2025-03-15 20:55:08 +0300

    edges group

---
+++ src/core/scheme/figures/FigureEdges.ts
@@ -0,0 +1,59 @@
+import { Position } from '../Dimensions';
+import { type SourceTreeNode } from '../SourceTreeNode';
+import { Edge } from './Edge';
+
+interface FigureEdgesOptions {
+    ctx: CanvasRenderingContext2D;
+    node: SourceTreeNode;
+    color?: string;
+}
+
+export class FigureEdges {
+    static create(options: FigureEdgesOptions) {
+        const { position, size, children } = options.node;
+
+        const edges: Array<Edge> = [];
+
+        const figureX = position.x + size.width / 2;
+        const figureY = position.y + size.height;
+
+        const lastIndex = this.isLastIndex(children);
+
+        children.forEach((child, index) => {
+            const x = child.position.x;
+            const y = child.position.y + child.size.height / 2;
+
+            const edgeStartPosition = new Position(x, y);
+
+            const childEdge = new Edge({
+                ctx: options.ctx,
+                position: edgeStartPosition,
+                strokeColor: options.color,
+                segment: true,
+            });
+
+            childEdge.lineTo(figureX, y);
+            edges.push(childEdge);
+
+            if (index === lastIndex) {
+                const position = new Position(figureX, figureY);
+                const figureEdge = new Edge({ ctx: options.ctx, position, strokeColor: options.color });
+
+                figureEdge.lineTo(figureX, y);
+                edges.push(figureEdge);
+            }
+        });
+
+        return edges;
+    }
+
+    private static isLastIndex(children: Array<SourceTreeNode>): number {
+        return children.length - 1;
+    }
+
+    static draw(edges: Array<Edge>) {
+        edges.forEach((edge) => {
+            edge.draw();
+        });
+    }
+}
