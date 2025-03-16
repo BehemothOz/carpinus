71ec93b304e16a0594da63990ceced3bc68aef41
behemothoz <behemothoz@yandex.ru>
2025-03-16 23:42:32 +0300

    buttons

--- src/carpinus.ts
+++ src/carpinus.ts
@@ -1,12 +1,14 @@
 import { CarpinusScene } from './core/CarpinusScene';
 import { source } from './__source';
 
-import { Downloader } from './core/downloader';
+// import { Downloader } from './core/downloader';
 
 export class Carpinus {
     constructor() {
         const sceneContainer = document.getElementById('scene') as HTMLDivElement;
-        const button = document.querySelector('.button') as HTMLButtonElement;
+
+        const buttonCenter = document.querySelector('.button-center') as HTMLButtonElement;
+        const buttonDownload = document.querySelector('.button-download') as HTMLButtonElement;
 
         const scene = new CarpinusScene({
             container: sceneContainer,
@@ -17,9 +19,12 @@ export class Carpinus {
 
         // const downloader = new Downloader(scene);
 
-        button.addEventListener('click', () => {
-            // scene.toCenter();
-            // downloader.download();
+        buttonCenter.addEventListener('click', () => {
+            console.log('buttonCenter clicked');
+        });
+
+        buttonDownload.addEventListener('click', () => {
+            console.log('buttonDownload clicked');
         });
     }
 }
