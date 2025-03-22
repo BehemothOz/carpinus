dc79222209f139b3570666a9be2507f6cca99583
behemothoz <behemothoz@yandex.ru>
2025-03-22 19:22:39 +0300

    actualization source

--- src/__source.ts
+++ src/__source.ts
@@ -9,30 +9,234 @@ export const source: SourceItem = {
     type: 'root',
     children: [
         {
-            name: 'Context 1',
+            name: 'General',
+            type: 'context',
+            children: [],
+        },
+        {
+            name: 'Acquisition',
             type: 'context',
             children: [
                 {
-                    name: 'Sub-Context 1.1',
+                    name: 'CaptchaErrorContext',
                     type: 'subcontext',
-                    children: [{ name: 'Sub-Context 1.1', type: 'subcontext', children: [] }],
+                    children: [
+                        {
+                            name: 'CaptchaError',
+                            type: 'feature',
+                            children: [],
+                        },
+                    ],
+                },
+                {
+                    name: 'RequestRequiresCaptcha',
+                    type: 'feature',
+                    children: [],
+                },
+                {
+                    name: 'ExternalReferral',
+                    type: 'feature',
+                    children: [],
+                },
+                {
+                    name: 'OTPErrorContext',
+                    type: 'subcontext',
+                    children: [
+                        {
+                            name: 'OTPError',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'OTPConfirmEmailPopupError',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'OTPConfirmPhonePopupError',
+                            type: 'feature',
+                            children: [],
+                        },
+                    ],
+                },
+                {
+                    name: 'MFAOTPErrorContext',
+                    type: 'subcontext',
+                    children: [
+                        {
+                            name: 'MFAOTPError',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'MFAOTPConfirmPhonePopupError',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'MFAOTPConfirmEmailPopupError',
+                            type: 'feature',
+                            children: [],
+                        },
+                    ],
+                },
+                {
+                    name: 'AuthUserErrorContext',
+                    type: 'subcontext',
+                    children: [
+                        {
+                            name: 'AuthViaOneTimeCodeError',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'AddNewEmailError',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'AddNewPhoneError',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'ConfirmEmailError',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'EditEmailError',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'ConfirmPhoneError',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'EditPhoneError',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'MergeUsersError',
+                            type: 'feature',
+                            children: [],
+                        },
+                    ],
+                },
+                {
+                    name: 'AdvCake',
+                    type: 'feature',
+                    children: [],
+                },
+                {
+                    name: 'UserCredentialPage',
+                    type: 'feature',
+                    children: [],
+                },
+                {
+                    name: 'PromocodeActivationByQueryParam',
+                    type: 'feature',
+                    children: [],
+                },
+                {
+                    name: 'AppsFlyer',
+                    type: 'feature',
+                    children: [],
+                },
+                {
+                    name: 'ReferalsCollector',
+                    type: 'feature',
+                    children: [],
+                },
+                {
+                    name: 'BDUI',
+                    type: 'feature',
+                    children: [],
+                },
+                {
+                    name: 'PromocodeLanding',
+                    type: 'subcontext',
+                    children: [
+                        {
+                            name: 'Page',
+                            type: 'feature',
+                            children: [],
+                        },
+                    ],
+                },
+                {
+                    name: 'ArtPromo',
+                    type: 'feature',
+                    children: [],
+                },
+                {
+                    name: 'ArtBuyCollection',
+                    type: 'feature',
+                    children: [],
                 },
             ],
         },
         {
-            name: 'Context 2',
+            name: 'Monetization',
             type: 'context',
             children: [
                 {
-                    name: 'Sub-Context 2.1 sdfsd fsd fs df',
+                    name: 'Payment',
                     type: 'subcontext',
                     children: [
-                        { name: 'Feature 2.1.1 sdf sdf sdfsdfsd fsd fs df sdf sdf sdf', type: 'feature', children: [] },
-                        { name: 'Feature 2.1.2', type: 'feature', children: [] },
+                        {
+                            name: 'ValueObject',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'PaymentFacade',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'PaymentEntryPoint',
+                            type: 'feature',
+                            children: [],
+                        },
+                        {
+                            name: 'PaymentFrame',
+                            type: 'feature',
+                            children: [],
+                        },
                     ],
                 },
-                { name: 'feature 2.1', type: 'feature', children: [] },
-                { name: 'Sub-Context 2.2', type: 'subcontext', children: [] },
+            ],
+        },
+        {
+            name: 'Navigation',
+            type: 'context',
+            children: [
+                {
+                    name: 'BookCard',
+                    type: 'feature',
+                    children: [],
+                },
+            ],
+        },
+        {
+            name: 'Loyalty',
+            type: 'context',
+            children: [
+                {
+                    name: 'Loyalty',
+                    type: 'subcontext',
+                    children: [
+                        {
+                            name: 'BonusesForPayingPpd',
+                            type: 'feature',
+                            children: [],
+                        },
+                    ],
+                },
             ],
         },
     ],
